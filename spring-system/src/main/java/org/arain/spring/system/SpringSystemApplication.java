e1348d0f5e75a90bf4b5828d39c01ed61488bde8
educational-system <2530728915@qq.com>
2018-10-25 15:48:35 +0800

    修改相关配置

--- spring-system/src/main/java/org/arain/spring/system/SpringSystemApplication.java
+++ spring-system/src/main/java/org/arain/spring/system/SpringSystemApplication.java
@@ -5,7 +5,11 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+/**
+ * 
+ * @author arain
+ * @date 2018年10月25日 下午3:33:00
+ */
 @SpringCloudApplication
 @EnableDiscoveryClient
 @EnableFeignClients
