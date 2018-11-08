74a418b683b6ce42497c0c7618017cadc312b736
educational-system <2530728915@qq.com>
2018-11-08 16:06:14 +0800

    提交

--- spring-config/src/main/java/org/arain/spring/config/SpringConfigApplication.java
+++ spring-config/src/main/java/org/arain/spring/config/SpringConfigApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+/**
+ * 
+ * @author arain
+ * @date 2018年10月29日 上午8:39:42
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
